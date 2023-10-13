6db0fb31e61cde32a3e91590787067a494960a23
Ben Lantaff <ben@benlantaff.com>
2023-10-13 17:18:26 -0400

    now able to save items

---
+++ models/item.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  unitOfMeasure: String,
+  sourcedFrom: String,
+  price: Number,
+});
+
+module.exports = mongoose.model("Item", itemSchema);
--- routes/webRouter.js
+++ routes/webRouter.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const { check, body } = require('express-validator');
+const express = require("express");
+const { check, body } = require("express-validator");
 const router = express.Router();
-const webController = require('../controllers/webController');
-const { routeProtection } = require('../middleware/customMiddleware');
+const webController = require("../controllers/webController");
+const { routeProtection } = require("../middleware/customMiddleware");
 
 // Index
-router.route('/').get(routeProtection,webController.renderHomePage);
-router.route('/items').get(routeProtection,webController.renderItemsPage);
+router.route("/").get(routeProtection, webController.renderHomePage);
+router.route("/items").get(routeProtection, webController.renderItemsPage);
+router.route("/createItem").post(routeProtection, webController.createItem);
 
 // Authentication
-router.route('/login').get(webController.renderLoginPage);
-router.route('/login').post(webController.postLogin);
-router.route('/logout').get(webController.getLogout);
+router.route("/login").get(webController.renderLoginPage);
+router.route("/login").post(webController.postLogin);
+router.route("/logout").get(webController.getLogout);
 
 // Create accounts
-router.route('/createAccount').get(webController.renderCreateAccountPage);
-router.route('/createAccount').post(
-  check('email').isEmail().withMessage('Please enter a valid email.'),
-  body('confirmPassword').custom((value, { req }) => {
+router.route("/createAccount").get(webController.renderCreateAccountPage);
+router.route("/createAccount").post(
+  check("email").isEmail().withMessage("Please enter a valid email."),
+  body("confirmPassword").custom((value, { req }) => {
     if (value !== req.body.password) {
-      throw new Error('Passwords must match');
+      throw new Error("Passwords must match");
     }
     return true;
   }),
@@ -27,20 +28,20 @@ router.route('/createAccount').post(
 );
 
 // Verify accounts
-router.route('/verifyEmail').get(webController.renderVerifyEmailPage);
-router.route('/verifyEmail').post(webController.postVerifyEmailPage);
+router.route("/verifyEmail").get(webController.renderVerifyEmailPage);
+router.route("/verifyEmail").post(webController.postVerifyEmailPage);
 
 // Profile
-router.route('/profile').get(routeProtection, webController.renderProfilePage);
+router.route("/profile").get(routeProtection, webController.renderProfilePage);
 
 // Reset password
-router.get('/reset', webController.getReset);
-router.post('/reset', webController.postReset);
-router.get('/reset/:token', webController.getNewPassword);
-router.post('/new-password', webController.postNewPassword);
+router.get("/reset", webController.getReset);
+router.post("/reset", webController.postReset);
+router.get("/reset/:token", webController.getNewPassword);
+router.post("/new-password", webController.postNewPassword);
 
 // Resend email verification code
-router.get('/resend', webController.getResend);
-router.post('/resend', webController.postResend);
+router.get("/resend", webController.getResend);
+router.post("/resend", webController.postResend);
 
 module.exports = router;
